cc902679ba10dcd113abf95c6f8ba77d5b4dfe02
shaun palmer <shaun.palmer0@gmail.com>
2020-05-18 21:28:05 +1200

    Tidying up the clutter
    Tidying up the clutter from research

---
+++ form-group.php
@@ -0,0 +1,117 @@
+<?php
+
+    <div class="form-group ">
+            <label for="property_type" class="property_type text-success">Property Type</label>
+    <select class="form-control" id="id_property_type" name="property_type">
+    <option value="8">House</option>
+    <option value="6">Apartment</option>
+    <option value="5">Townhouse</option>
+    <option value="3">Unit</option>
+    <option value="9">Lifestyle</option>
+    </select>
+
+        </div>
+        <div class="form-group ">
+                            <label for="bedroom_num" class="bedroom_num text-info">How many bedroom?</label>
+                <select class="form-control" id="id_bedroom_num" name="bedroom_num"><option value="3">3 Bedrooms</option><option value="1">1 Bedroom</option><option value="2">2 Bedrooms</option><option value="4">4 Bedrooms</option><option value="5">5 Bedrooms</option><option value="6">6+ Bedrooms</option></select>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="bathroom_num" class="bathroom_num text-success">How many bathroom?</label>
+        <select class="form-control" id="id_bathroom_num" name="bathroom_num">
+        <option value="1">1 Bathroom</option>
+        <option value="2">2 Bathrooms</option>
+        <option value="3">3 Bathrooms</option>
+        <option value="4">4 Bathrooms</option>
+        <option value="5">5 Bathrooms</option>
+        <option value="6">6+ Bathrooms</option>
+        </select>
+            </div>
+
+            // the Suburb we workin 
+            <select name="suburb_id" id="select_suburb_id" class="form-control">
+            <option value="160301">Addington</option>
+            <option value="160302">Aidanfield</option>
+            <option value="160303">Aranui</option>
+            <option value="160304">Avondale</option>
+            <option value="160305">Avonhead</option>
+            <option value="160306">Avonside</option>
+            <option value="160307">Beckenham</option>
+            <option value="160308">Belfast</option>
+            <option value="160309">Bexley</option>
+            <option> value="160310">Bishopdale</option>
+            <option value="160311">Bromley</option>
+            <option> value="160312">Brooklands</option>
+            <option value="160313">Broomfield</option>
+            <option> value="160314">Bryndwr</option>
+            <option value="160315">Burnside</option>
+            <option> value="160316">Burwood</option>
+            <option value="160317">Casebrook</option>
+            <option> value="160318">Cashmere</option>
+            <option value="160319">Christchurch Airport</option>
+            <option value="160320">Christchurch Central</option>
+            <option value="160321">Christchurch Surrounds</option>
+            <option value="160322">Clifton</option>
+            <option value="160323">Coutts Island</option>
+            <option value="160324">Cracroft</option>
+            <option value="160325">Dallington</option>
+            <option value="160326">Edgeware</option>
+            <option value="160327">Fendalton</option>
+            <option value="160328">Ferrymead</option>
+            <option value="160329">Halswell</option>
+            <option value="160330">Harewood</option>
+            <option value="160331">Heathcote Valley</option>
+            <option value="160332">Hei Hei</option>
+            <option value="160333">Hillmorton</option>
+            <option value="160334">Hillsborough</option>
+            <option value="160335">Hoon Hay</option>
+            <option value="160336">Hornby</option>
+            <option value="160337">Huntsbury</option>
+            <option value="160338">Ilam</option>
+            <option value="160339">Islington</option>
+            <option value="160340">Kainga</option>
+            <option value="160341">Kennedys Bush</option>
+            <option value="160342">Linwood</option>
+            <option value="160343">Mairehau</option>
+            <option value="160344">Marshland</option>
+            <option value="160345">Merivale</option>
+            <option value="160346">Middleton</option>
+            <option value="160347">Moncks Bay</option>
+            <option value="160348">Mount Pleasant</option>
+            <option value="160349">New Brighton</option>
+            <option value="160350">North New Brighton</option>
+            <option value="160351">Northcote</option>
+            <option value="160352">Northwood</option>
+            <option value="160353">Opawa</option>
+            <option value="160354">Papanui</option>
+            <option value="160355">Parklands</option>
+            <option value="160356">Phillipstown</option>
+            <option value="160357">Redcliffs</option>
+            <option value="160358">Redwood</option>
+            <option value="160359">Riccarton</option>
+            <option value="160360">Richmond</option>
+            <option value="160361">Richmond Hill</option>
+            <option value="160362">Russley</option>
+            <option value="160363">Saint Albans</option>
+            <option value="160364">Saint Martins</option>
+            <option value="160365">Shirley</option>
+            <option value="160366">Sockburn</option>
+            <option value="160367">Somerfield</option>
+            <option value="160368">South New Brighton</option>
+            <option value="160369">Southshore</option>
+            <option value="160370">Spencerville</option>
+            <option value="160371">Spreydon</option>
+            <option value="160372">Strowan</option>
+            <option value="160373">Styx</option>
+            <option value="160374">Sumner</option>
+            <option value="160375">Sydenham</option>
+            <option value="160376">Templeton</option>
+            <option value="160377">Upper Riccarton</option>
+            <option value="160378">Waimairi Beach</option>
+            <option value="160379">Wainoni</option>
+            <option value="160380">Waltham</option>
+            <option value="160381">Westmorland</option>
+            <option value="160382">Wigram</option>
+            <option value="160383">Woolston</option>
+            <option value="160384">Yaldhurst</option>
+            </select>
